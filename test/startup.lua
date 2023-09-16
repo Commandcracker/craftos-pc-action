f2872acf887c812b6ed9132f65330665a7c03093
Commandcracker <lukas.fricke.dev@gmail.com>
2023-09-17 00:58:24 +0200

    Exit on completion

--- test/startup.lua
+++ test/startup.lua
@@ -32,3 +32,5 @@ print("ComputerID:", os.getComputerID())
 print("bye :)")
 
 os.shutdown()
+-- Using shutdown is preferred, because it will quit the program faster than shell.exit() and
+-- It makes it possible to exit the program with any error code by using os.shutdown(error_code)
